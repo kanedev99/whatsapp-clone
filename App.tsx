cd0c487f87333791bca1485d021531394742710f
Kane <thotrinhbku@gmail.com>
2021-04-29 14:00:20 +0700

    Implement register account

--- App.tsx
+++ App.tsx
@@ -1,18 +1,70 @@
+import Amplify, { graphqlOperation } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import { withAuthenticator } from 'aws-amplify-react-native'
-import Amplify from 'aws-amplify'
-import config from './src/aws-exports'
+import config from './src/aws-exports';
+import {
+  Auth,
+  API
+} from 'aws-amplify';
+import { getUser } from './src/graphql/queries';
+import { createUser, updateUser } from './src/graphql/mutations';
 Amplify.configure(config)
 
 
 function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+
+  const avtListUri = [
+    "https://hieumobile.com/wp-content/uploads/avatar-among-us-1.jpg",
+    "https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg",
+    "https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg",
+    "https://hieumobile.com/wp-content/uploads/avatar-among-us-5.jpg",
+    "https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg",
+    "https://hieumobile.com/wp-content/uploads/avatar-among-us-7.jpg",
+    "https://hieumobile.com/wp-content/uploads/avatar-among-us-8.jpg",
+    "https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg",
+]
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      // Get authenticated from Auth
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+
+      if (userInfo) {
+        // Get the user from Backend with the user Id from Auth
+        const userData = await API.graphql(
+          graphqlOperation(
+            getUser,
+            { id: userInfo.attributes.sub }
+          )
+        )
+
+        if (userData) {
+          console.log("User already registerd!");
+        }
+
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri: avtListUri[Math.floor(Math.random()*9)],
+          status: "Hey, I am using WhatsApp"
+        }
+
+        // If there is no user in Db with the Id, then create one
+        await API.graphql(
+          graphqlOperation(
+            createUser,
+            {input: newUser}
+          )
+        )
+      }
+    }
+    fetchUser();
+  }, [])
 
   if (!isLoadingComplete) {
     return null;
